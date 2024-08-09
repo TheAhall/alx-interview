a5575913526c4a61839ec21b08767e79edfeb83e
TheAhall <81965922+TheAhall@users.noreply.github.com>
2024-08-09 02:34:37 +0100

    0x02-minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,11 @@ Minimum operations to get exactly n 'H' characters
 def minOperations(n):
     if n <= 1:
         return 0
-    
     operations = 0
     divisor = 2
-
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
     return operations
